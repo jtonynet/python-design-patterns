ec7f3bc8501824783850c9831f937727dfedb03e
Jtony <jtony.net@gmail.com>
2018-01-29 21:52:02 -0200

    initial commit

---
+++ calculador_de_descontos.py
@@ -0,0 +1,7 @@
+# -*- coding: UTF-8 -*-
+class Calculador_de_descontos(object):
+    def calcula(orcamento):
+        if orcamento.total_de_itens > 5:
+            return orcamento.valor = 0.1
+        elif orcamento.valor > 500:
+            return orcamento.valor = 0.07
--- orcamento.py
+++ orcamento.py
@@ -1,5 +1,28 @@
+# -*- coding: UTF-8 -*-
 class Orcamento(object):
     def __init__(self, valor):
+        self.__itens = []
+
+    @property
+    def valor(self):
+        total = 0.0
+        for item in self.__itens:
+            total += item.valor
+        return self.__valor
+
+    def obter_itens(self):
+        return tuple(self.__itens)
+
+    @property
+    def total_itens(self):
+        return len(self.__itens)
+
+    def adiciona_item(self, item):
+        self.__itens.append(item)
+
+class Item(object):
+    def __init__(self, nome, valor):
+        self.__nome = nome
         self.__valor = valor
 
     @property
